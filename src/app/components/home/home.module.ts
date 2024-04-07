70c3f72facb1f18888e16072800170e4f80e900f
vinipolitta <vinicius.politta1@gmail.com>
2024-04-06 23:03:10 -0300

    feat: create page contact

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -5,12 +5,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutComponent } from '../about/about.component';
 import { ServicesComponent } from '../services/services.component';
 import { PortifolioComponent } from '../portifolio/portifolio.component';
+import { ContactComponent } from '../contact/contact.component';
 
 const components = [
   HomeComponent,
   AboutComponent,
   ServicesComponent,
   PortifolioComponent,
+  ContactComponent,
+
 
 ]
 
