22ad3468463b476886682b49ac736094504beda0
vinipolitta <vinicius.politta1@gmail.com>
2024-04-06 22:08:38 -0300

    feat: create page services

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutComponent } from '../about/about.component';
+import { ServicesComponent } from '../services/services.component';
+import { PortifolioComponent } from '../portifolio/portifolio.component';
 
 const components = [
   HomeComponent,
   AboutComponent,
+  ServicesComponent,
+  PortifolioComponent,
+
 ]
 
 
---
+++ src/app/components/portifolio/portifolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  templateUrl: './portifolio.component.html',
+  styleUrls: ['./portifolio.component.scss']
+})
+export class PortifolioComponent {
+
+}
